cf139bcaed57223342d5eac72ece37e49a10b84a
Carlos Ruiz <Carlos.Ruiz@AIDecisions.com>
2024-07-23 06:59:55 -0400

    updating app.py and sqlHelper.py

--- app.py
+++ app.py
@@ -15,12 +15,31 @@ sql = SQLHelper()
 @app.route('/')
 def welcome():
     return ('''
-            Welcome to the Climate Analysis API!                                                        <br/>
-            Available Routes:                                                                           <br/>
-            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/analysis1_orm">/api/v1.0/full_data_sql</a>       <br/>
-            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/analysis1_sql">/api/v1.0/full_data_orm</a>       <br/>
-            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/analysis1_orm">/api/v1.0/full_data_sql</a>       <br/>
-            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/analysis1_sql">/api/v1.0/full_data_orm</a>       <br/>
+            Welcome to the Climate Analysis API!                                                                                                                                            <br/>
+            Available Routes:                                                                                                                                                               <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/full_data_sql">/api/v1.0/full_data_sql</a>                                                                                           <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/full_data_orm">/api/v1.0/full_data_orm</a>                                                                                           <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/reached_goal_sql">/api/v1.0/reached_goal_sql</a>                                                                                     <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/reached_goal_orm">/api/v1.0/reached_goal_orm</a>                                                                                     <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/count_of_categories_by_outcome_sql">/api/v1.0/count_of_categories_by_outcome_sql</a>                                                 <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/count_of_categories_by_outcome_orm">/api/v1.0/count_of_categories_by_outcome_orm</a>                                                 <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/count_of_subcategories_by_outcome_sql">/api/v1.0/count_of_subcategories_by_outcome_sql</a>                                           <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/count_of_subcategories_by_outcome_orm">/api/v1.0/count_of_subcategories_by_outcome_orm</a>                                           <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/percentage_goal_by_country_sql">/api/v1.0/percentage_goal_by_country_sql</a>                                                         <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/percentage_goal_by_country_orm">/api/v1.0/percentage_goal_by_country_orm</a>                                                         <br/>
+                                                                                                                                                                                            <br/>
+                                                                                                                                                                                            <br/>
+            Explanation of the Routes:                                                                                                                                                      <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;full_data_sql: Returns all the data from the table using SQL.                                                                                           <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;full_data_orm: Returns all the data from the table using ORM.                                                                                           <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;reached_goal_sql: Returns the number of projects that have reached their funding goal and have been staff picked or are in the spotligh using SQL.      <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;reached_goal_orm: Returns the number of projects that have reached their funding goal and have been staff picked or are in the spotligh using ORM.      <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;count_of_categories_by_outcome_sql: Returns the count of projects by category and outcome using SQL.                                                    <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;count_of_categories_by_outcome_orm: Returns the count of projects by category and outcome using ORM.                                                    <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;count_of_subcategories_by_outcome_sql: Returns the count of projects by subcategory and outcome using SQL.                                              <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;count_of_subcategories_by_outcome_orm: Returns the count of projects by subcategory and outcome using ORM.                                              <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;percentage_goal_by_country_sql: Returns the percentage of projects that have reached their funding goal by country using SQL.                           <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;percentage_goal_by_country_orm: Returns the percentage of projects that have reached their funding goal by country using ORM.                           <br/>
             ''')
 
 
@@ -36,28 +55,52 @@ def full_data_orm():
     return (jsonify(data))
 
 
-# @app.route("/api/v1.0/analysis1_sql")
-# def analysis1_sql():
-#     data = sql.query_analysis1_sql()
-#     return (jsonify(data))
+@app.route("/api/v1.0/reached_goal_sql")
+def reached_goal_sql():
+    data = sql.reached_goal_sql()
+    return (jsonify(data))
 
 
-# @app.route("/api/v1.0/analysis1_orm")
-# def analysis1_orm():
-#     data = sql.query_analysis1_orm()
-#     return (jsonify(data))
+@app.route("/api/v1.0/reached_goal_orm")
+def reached_goal_orm():
+    data = sql.reached_goal_orm()
+    return (jsonify(data))
 
 
-# @app.route("/api/v1.0/analysis2_sql")
-# def analysis1_sql():
-#     data = sql.query_analysis2_sql()
-#     return (jsonify(data))
+@app.route("/api/v1.0/count_of_categories_by_outcome_sql")
+def count_of_categories_by_outcome_sql():
+    data = sql.count_of_categories_by_outcome_sql()
+    return (jsonify(data))
 
 
-# @app.route("/api/v1.0/analysis2_orm")
-# def analysis1_orm():
-#     data = sql.query_analysis2_orm()
-#     return (jsonify(data))
+@app.route("/api/v1.0/count_of_categories_by_outcome_orm")
+def count_of_categories_by_outcome_orm():
+    data = sql.count_of_categories_by_outcome_orm()
+    return (jsonify(data))
+
+
+@app.route("/api/v1.0/count_of_subcategories_by_outcome_sql")
+def count_of_subcategories_by_outcome_sql():
+    data = sql.count_of_subcategories_by_outcome_sql()
+    return (jsonify(data))
+
+
+@app.route("/api/v1.0/count_of_subcategories_by_outcome_orm")
+def count_of_subcategories_by_outcome_orm():
+    data = sql.count_of_subcategories_by_outcome_orm()
+    return (jsonify(data))
+
+
+@app.route("/api/v1.0/percentage_goal_by_country_sql")
+def percentage_goal_by_country_sql():
+    data = sql.percentage_goal_by_country_sql()
+    return (jsonify(data))
+
+
+@app.route("/api/v1.0/percentage_goal_by_country_orm")
+def percentage_goal_by_country_orm():
+    data = sql.percentage_goal_by_country_orm()
+    return (jsonify(data))
 
 
 # Run the App
--- sqlHelper.py
+++ sqlHelper.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, text, func
+from sqlalchemy import create_engine, text, func, case
 import pandas as pd
 import datetime as dt
 import config as cfg
@@ -76,15 +76,20 @@ class SQLHelper():
         full_data_sql_df = pd.read_sql(text(query), con=self.engine)
         data = full_data_sql_df.to_dict(orient="records")
         return (data)
-    
+
+
     def full_data_orm(self):
         # Save reference to the table
-        full_data_orm = self.Base.classes.full_data_orm
+        # Assign the classes to variables
+        Contact = self.Base.classes.contact
+        Campaign = self.Base.classes.campaign
+        Category = self.Base.classes.category
+        Subcategory = self.Base.classes.subcategory
 
         # Create our session (link) from Python to the DB
         session = Session(self.engine)
 
-        full_data_orm = session.query(
+        results = session.query(
             Contact.first_name,
             Contact.last_name,
             Contact.email,
@@ -110,96 +115,312 @@ class SQLHelper():
             Subcategory, Campaign.subcategory_id == Subcategory.subcategory_id
         ).all()
 
+        # Save the query results as a Pandas DataFrame. Explicitly set the column names
+        full_data_orm_df = pd.DataFrame(results, columns=['first_name', 'last_name', 'email', 'company_name', 'description', 'goal', 'pledged', 'outcome', 'backers_count', 'country', 'currency', 'launched_date', 'end_date', 'staff_pick', 'spotlight', 'category', 'subcategory'])
+
         # close session
         session.close()
 
         # Convert the query results to a Dictionary
-        data = analysis1_df.to_dict(orient="records")
+        data = full_data_orm_df.to_dict(orient="records")
         return (data)
 
-    # def reached_goal_sql(self):
-    #     # Find the most recent date in the data set.
-    #     query = """
-    #             SELECT
-    #                 cont.first_name,
-    #                 cont.last_name,
-    #                 cont.email,
-    #                 camp.company_name,
-    #                 camp.description,
-    #                 camp.goal,
-    #                 camp.pledged,
-    #                 camp.outcome,
-    #                 camp.backers_count,
-    #                 camp.country,
-    #                 camp.currency,
-    #                 camp.launched_date,
-    #                 camp.end_date,
-    #                 camp.staff_pick,
-    #                 camp.spotlight,
-    #                 cat.category,
-    #                 sub.subcategory
-    #             FROM
-    #                 public.contact cont
-    #             INNER JOIN public.campaign camp
-    #                 ON cont.contact_id = camp.contact_id
-    #             INNER JOIN public.category cat
-    #                 ON camp.category_id = cat.category_id
-    #             INNER JOIN public.subcategory sub
-    #                 ON camp.subcategory_id = sub.subcategory_id
-    #             WHERE
-    #                 (staff_pick = TRUE OR spotlight = TRUE)
-    #                 AND pledged >= goal
-    #             ;
-    #             """
 
-    #     # Save the query results as a Pandas DataFrame
-    #     reached_goal_sql_df = pd.read_sql(text(query), con=self.engine)
-    #     data = reached_goal_sql_df.to_dict(orient="records")
-    #     return (data)
-    
-    # def reached_goal_orm(self):
-    #     # Save reference to the table
-    #     reached_goal_orm = self.Base.classes.analysis1
+    def reached_goal_sql(self):
+        # Find the most recent date in the data set.
+        query = """
+                SELECT
+                    cont.first_name,
+                    cont.last_name,
+                    cont.email,
+                    camp.company_name,
+                    camp.description,
+                    camp.goal,
+                    camp.pledged,
+                    camp.outcome,
+                    camp.backers_count,
+                    camp.country,
+                    camp.currency,
+                    camp.launched_date,
+                    camp.end_date,
+                    camp.staff_pick,
+                    camp.spotlight,
+                    cat.category,
+                    sub.subcategory
+                FROM
+                    public.contact cont
+                INNER JOIN public.campaign camp
+                    ON cont.contact_id = camp.contact_id
+                INNER JOIN public.category cat
+                    ON camp.category_id = cat.category_id
+                INNER JOIN public.subcategory sub
+                    ON camp.subcategory_id = sub.subcategory_id
+                WHERE
+                    (staff_pick = TRUE OR spotlight = TRUE)
+                    AND pledged >= goal
+                ;
+                """
 
-    #     # Create our session (link) from Python to the DB
-    #     session = Session(self.engine)
+        # Save the query results as a Pandas DataFrame
+        reached_goal_sql_df = pd.read_sql(text(query), con=self.engine)
+        data = reached_goal_sql_df.to_dict(orient="records")
+        return (data)
 
 
+    def reached_goal_orm(self):
+        # Save reference to the table
+        # Assign the classes to variables
+        Contact = self.Base.classes.contact
+        Campaign = self.Base.classes.campaign
+        Category = self.Base.classes.category
+        Subcategory = self.Base.classes.subcategory
 
-    #     # close session
-    #     session.close()
+        # Create our session (link) from Python to the DB
+        session = Session(self.engine)
 
-    #     # Convert the query results to a Dictionary
-    #     data = reached_goal_orm_df.to_dict(orient="records")
-    #     return (data)
-    
-    #     def query_analysis1_sql(self):
-    #     # Find the most recent date in the data set.
-    #     query = """
-    #             SELECT 
-    #             FROM 
-    #             WHERE 
-    #             ORDER BY ;
-    #             """
+        # ORM to get all campaigns that have been staff picked or are in the spotlight and have reached their goal
+        results = session.query(
+            Contact.first_name,
+            Contact.last_name,
+            Contact.email,
+            Campaign.company_name,
+            Campaign.description,
+            Campaign.goal,
+            Campaign.pledged,
+            Campaign.outcome,
+            Campaign.backers_count,
+            Campaign.country,
+            Campaign.currency,
+            Campaign.launched_date,
+            Campaign.end_date,
+            Campaign.staff_pick,
+            Campaign.spotlight,
+            Category.category,
+            Subcategory.subcategory
+        ).join(
+            Campaign, Contact.contact_id == Campaign.contact_id
+        ).join(
+            Category, Campaign.category_id == Category.category_id
+        ).join(
+            Subcategory, Campaign.subcategory_id == Subcategory.subcategory_id
+        ).filter(
+            (Campaign.staff_pick == True) | (Campaign.spotlight == True),
+            Campaign.pledged >= Campaign.goal
+        ).all()
 
-    #     # Save the query results as a Pandas DataFrame
-    #     analysis1_df = pd.read_sql(text(query), con=self.engine)
-    #     data = analysis1_df.to_dict(orient="records")
-    #     return (data)
-    
-    # def query_analysis1_orm(self):
-    #     # Save reference to the table
-    #     analysis1 = self.Base.classes.analysis1
+        # Save the query results as a Pandas DataFrame. Explicitly set the column names
+        reached_goal_orm_df = pd.DataFrame(results, columns=['first_name', 'last_name', 'email', 'company_name', 'description', 'goal', 'pledged', 'outcome', 'backers_count', 'country', 'currency', 'launched_date', 'end_date', 'staff_pick', 'spotlight', 'category', 'subcategory'])
 
-    #     # Create our session (link) from Python to the DB
-    #     session = Session(self.engine)
+        # close session
+        session.close()
+
+        # Convert the query results to a Dictionary
+        data = reached_goal_orm_df.to_dict(orient="records")
+        return (data)
 
 
+    def count_of_categories_by_outcome_sql(self):
+        # Find the most recent date in the data set.
+        query = """
+                SELECT
+                    category,
+                    SUM(canceled) AS canceled,
+                    SUM(failed) AS failed,
+                    SUM(live) AS live,
+                    SUM(successful) AS successful,
+                    COUNT(category) AS grand_total
+                FROM
+                    (
+                    SELECT
+                        cat.category,
+                        CASE WHEN camp.outcome = 'canceled' then 1 else 0 END AS canceled,
+                        CASE WHEN camp.outcome = 'failed' then 1 else 0 END AS failed,
+                        CASE WHEN camp.outcome = 'live' then 1 else 0 END AS live,
+                        CASE WHEN camp.outcome = 'successful' then 1 else 0 END AS successful
+                    FROM
+                        public.campaign camp
+                    INNER JOIN public.category cat
+                        ON cat.category_id = camp.category_id
+                    )
+                GROUP BY
+                    category
+                ;
+                """
 
-    #     # close session
-    #     session.close()
+        # Save the query results as a Pandas DataFrame
+        count_of_categories_by_outcome_sql_df = pd.read_sql(text(query), con=self.engine)
+        data = count_of_categories_by_outcome_sql_df.to_dict(orient="records")
+        return (data)
 
-    #     # Convert the query results to a Dictionary
-    #     data = analysis1_df.to_dict(orient="records")
-    #     return (data)
+
+    def count_of_categories_by_outcome_orm(self):
+        # Save reference to the table
+        # Assign the classes to variables
+        Campaign = self.Base.classes.campaign
+        Category = self.Base.classes.category
+
+        # Create our session (link) from Python to the DB
+        session = Session(self.engine)
+
+        # ORM to get all campaigns that have been staff picked or are in the spotlight and have reached their goal
+        results = session.query(
+            Category.category,
+            func.sum(case((Campaign.outcome == 'canceled', 1), else_=0)).label('canceled'),
+            func.sum(case((Campaign.outcome == 'failed', 1), else_=0)).label('failed'),
+            func.sum(case((Campaign.outcome == 'live', 1), else_=0)).label('live'),
+            func.sum(case((Campaign.outcome == 'successful', 1), else_=0)).label('successful'),
+            func.count(Category.category).label('grand_total')
+        ).join(
+            Campaign, Category.category_id == Campaign.category_id
+        ).group_by(
+            Category.category
+        ).all()
+
+        # Save the query results as a Pandas DataFrame. Explicitly set the column names
+        count_of_categories_by_outcome_orm_df = pd.DataFrame(results, columns=['category', 'canceled', 'failed', 'live', 'successful', 'grand_total'])
+
+        # close session
+        session.close()
+
+        # Convert the query results to a Dictionary
+        data = count_of_categories_by_outcome_orm_df.to_dict(orient="records")
+        return (data)
+
+
+    def count_of_subcategories_by_outcome_sql(self):
+        # Find the most recent date in the data set.
+        query = """
+                SELECT
+                    subcategory,
+                    SUM(canceled) AS canceled,
+                    SUM(failed) AS failed,
+                    SUM(live) AS live,
+                    SUM(successful) AS successful,
+                    COUNT(subcategory) AS grand_total
+                FROM
+                    (
+                    SELECT
+                        sub.subcategory,
+                        CASE WHEN camp.outcome = 'canceled' then 1 else 0 END AS canceled,
+                        CASE WHEN camp.outcome = 'failed' then 1 else 0 END AS failed,
+                        CASE WHEN camp.outcome = 'live' then 1 else 0 END AS live,
+                        CASE WHEN camp.outcome = 'successful' then 1 else 0 END AS successful
+                    FROM
+                        public.campaign camp
+                    INNER JOIN public.subcategory sub
+                        ON sub.subcategory_id = camp.subcategory_id
+                    )
+                GROUP BY
+                    subcategory
+                ;
+                """
+
+        # Save the query results as a Pandas DataFrame
+        full_data_sql_df = pd.read_sql(text(query), con=self.engine)
+        data = full_data_sql_df.to_dict(orient="records")
+        return (data)
+
+
+    def count_of_subcategories_by_outcome_orm(self):
+        # Save reference to the table
+        # Assign the classes to variables
+        Campaign = self.Base.classes.campaign
+        Subcategory = self.Base.classes.subcategory
+
+        # Create our session (link) from Python to the DB
+        session = Session(self.engine)
+
+        results = session.query(
+            Subcategory.subcategory,
+            func.sum(case((Campaign.outcome == 'canceled', 1), else_=0)).label('canceled'),
+            func.sum(case((Campaign.outcome == 'failed', 1), else_=0)).label('failed'),
+            func.sum(case((Campaign.outcome == 'live', 1), else_=0)).label('live'),
+            func.sum(case((Campaign.outcome == 'successful', 1), else_=0)).label('successful'),
+            func.count(Subcategory.subcategory).label('grand_total')
+        ).join(
+            Campaign, Subcategory.subcategory_id == Campaign.subcategory_id
+        ).group_by(
+            Subcategory.subcategory
+        ).all()
+
+        # Save the query results as a Pandas DataFrame. Explicitly set the column names
+        count_of_subcategories_by_outcome_orm_df = pd.DataFrame(results, columns=['subcategory', 'canceled', 'failed', 'live', 'successful', 'grand_total'])
+
+        # close session
+        session.close()
+
+        # Convert the query results to a Dictionary
+        data = count_of_subcategories_by_outcome_orm_df.to_dict(orient="records")
+        return (data)
+
+
+    def percentage_goal_by_country_sql(self):
+        # Find the most recent date in the data set.
+        query = """
+                SELECT
+                    country,
+                    SUM(perfect) AS goalreached,
+                    SUM(ninety) AS ninetypercent_goalreached,
+                    SUM(eighty) AS eightypercent_goalreached,
+                    SUM(seventy) AS seventypercent_goalreached,
+                    SUM(sixty) AS sixtypercent_goalreached,
+                    SUM(fifty) AS fiftypercent_goalreached,
+                    SUM(fail) AS lessthanfiftypercent_goalreached,
+                    COUNT(Country) AS grand_total
+                FROM
+                    (
+                    SELECT
+                        country,
+                        CASE WHEN pledged >= goal then 1 else 0 END AS perfect,
+                        CASE WHEN pledged >= goal * 0.9 AND pledged < goal then 1 else 0 END AS ninety,
+                        CASE WHEN pledged >= goal * 0.8 AND pledged < goal * 0.9 then 1 else 0 END AS eighty,
+                        CASE WHEN pledged >= goal * 0.7 AND pledged < goal * 0.8 then 1 else 0 END AS seventy,
+                        CASE WHEN pledged >= goal * 0.6 AND pledged < goal * 0.7 then 1 else 0 END AS sixty,
+                        CASE WHEN pledged >= goal * 0.5 AND pledged < goal * 0.6 then 1 else 0 END AS fifty,
+                        CASE WHEN pledged < goal * 0.5 then 1 else 0 END AS fail
+                    FROM
+                        campaign
+                    )
+                GROUP BY
+                    country
+                ;
+                """
+
+        # Save the query results as a Pandas DataFrame
+        percentage_goal_by_country_sql_df = pd.read_sql(text(query), con=self.engine)
+        data = percentage_goal_by_country_sql_df.to_dict(orient="records")
+        return (data)
+
+
+    def percentage_goal_by_country_orm(self):
+        # Save reference to the table
+        # Assign the classes to variables
+        Campaign = self.Base.classes.campaign
+
+        # Create our session (link) from Python to the DB
+        session = Session(self.engine)
+
+        results = session.query(
+            Campaign.country,
+            func.sum(case((Campaign.pledged >= Campaign.goal, 1), else_=0)).label('goalreached'),
+            func.sum(case(((Campaign.pledged >= Campaign.goal * 0.9) & (Campaign.pledged < Campaign.goal), 1), else_=0)).label('ninetypercent_goalreached'),
+            func.sum(case(((Campaign.pledged >= Campaign.goal * 0.8) & (Campaign.pledged < Campaign.goal * 0.9), 1), else_=0)).label('eightypercent_goalreached'),
+            func.sum(case(((Campaign.pledged >= Campaign.goal * 0.7) & (Campaign.pledged < Campaign.goal * 0.8), 1), else_=0)).label('seventypercent_goalreached'),
+            func.sum(case(((Campaign.pledged >= Campaign.goal * 0.6) & (Campaign.pledged < Campaign.goal * 0.7), 1), else_=0)).label('sixtypercent_goalreached'),
+            func.sum(case(((Campaign.pledged >= Campaign.goal * 0.5) & (Campaign.pledged < Campaign.goal * 0.6), 1), else_=0)).label('fiftypercent_goalreached'),
+            func.sum(case((Campaign.pledged < Campaign.goal * 0.5, 1), else_=0)).label('lessthanfiftypercent_goalreached'),
+            func.count(Campaign.country).label('grand_total')
+        ).group_by(
+            Campaign.country
+        ).all()
+
+        # Save the query results as a Pandas DataFrame. Explicitly set the column names
+        percentage_goal_by_country_orm_df = pd.DataFrame(results, columns=['country', 'goalreached', 'ninetypercent_goalreached', 'eightypercent_goalreached', 'seventypercent_goalreached', 'sixtypercent_goalreached', 'fiftypercent_goalreached', 'lessthanfiftypercent_goalreached', 'grand total'])
+
+        # close session
+        session.close()
+
+        # Convert the query results to a Dictionary
+        data = percentage_goal_by_country_orm_df.to_dict(orient="records")
+        return (data)
 

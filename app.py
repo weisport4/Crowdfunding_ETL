10ac13aced3b1753658a83819863ec581b01c9f6
Carlos Ruiz <Carlos.Ruiz@AIDecisions.com>
2024-07-22 22:02:06 -0400

    updating latest copy

--- app.py
+++ app.py
@@ -17,21 +17,47 @@ def welcome():
     return ('''
             Welcome to the Climate Analysis API!                                                        <br/>
             Available Routes:                                                                           <br/>
-            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/analysis1_orm">/api/v1.0/analysis1_orm</a>       <br/>
-            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/analysis1_sql">/api/v1.0/analysis1_sql</a>       <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/analysis1_orm">/api/v1.0/full_data_sql</a>       <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/analysis1_sql">/api/v1.0/full_data_orm</a>       <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/analysis1_orm">/api/v1.0/full_data_sql</a>       <br/>
+            &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/analysis1_sql">/api/v1.0/full_data_orm</a>       <br/>
             ''')
 
 
-@app.route("/api/v1.0/analysis1_orm")
-def analysis1_orm():
-    data = sql.query_analysis1_orm()
+@app.route("/api/v1.0/full_data_sql")
+def full_data_sql():
+    data = sql.full_data_sql()
     return (jsonify(data))
 
 
-@app.route("/api/v1.0/analysis1_sql")
-def analysis1_sql():
-    data = sql.query_analysis1_sql()
+@app.route("/api/v1.0/full_data_orm")
+def full_data_orm():
+    data = sql.full_data_orm()
     return (jsonify(data))
+
+
+# @app.route("/api/v1.0/analysis1_sql")
+# def analysis1_sql():
+#     data = sql.query_analysis1_sql()
+#     return (jsonify(data))
+
+
+# @app.route("/api/v1.0/analysis1_orm")
+# def analysis1_orm():
+#     data = sql.query_analysis1_orm()
+#     return (jsonify(data))
+
+
+# @app.route("/api/v1.0/analysis2_sql")
+# def analysis1_sql():
+#     data = sql.query_analysis2_sql()
+#     return (jsonify(data))
+
+
+# @app.route("/api/v1.0/analysis2_orm")
+# def analysis1_orm():
+#     data = sql.query_analysis2_orm()
+#     return (jsonify(data))
 
 
 # Run the App
--- sqlHelper.py
+++ sqlHelper.py
@@ -40,14 +40,75 @@ class SQLHelper():
     # Database Queries
     #################################################
 
-    def query_analysis1_orm(self):
+    def full_data_sql(self):
+        # Find the most recent date in the data set.
+        query = """
+                SELECT
+                    cont.first_name,
+                    cont.last_name,
+                    cont.email,
+                    camp.company_name,
+                    camp.description,
+                    camp.goal,
+                    camp.pledged,
+                    camp.outcome,
+                    camp.backers_count,
+                    camp.country,
+                    camp.currency,
+                    camp.launched_date,
+                    camp.end_date,
+                    camp.staff_pick,
+                    camp.spotlight,
+                    cat.category,
+                    sub.subcategory
+                FROM
+                    public.contact cont
+                INNER JOIN public.campaign camp
+                    ON cont.contact_id = camp.contact_id
+                INNER JOIN public.category cat
+                    ON camp.category_id = cat.category_id
+                INNER JOIN public.subcategory sub
+                    ON camp.subcategory_id = sub.subcategory_id
+                ;
+                """
+
+        # Save the query results as a Pandas DataFrame
+        full_data_sql_df = pd.read_sql(text(query), con=self.engine)
+        data = full_data_sql_df.to_dict(orient="records")
+        return (data)
+    
+    def full_data_orm(self):
         # Save reference to the table
-        analysis1 = self.Base.classes.analysis1
+        full_data_orm = self.Base.classes.full_data_orm
 
         # Create our session (link) from Python to the DB
         session = Session(self.engine)
 
-
+        full_data_orm = session.query(
+            Contact.first_name,
+            Contact.last_name,
+            Contact.email,
+            Campaign.company_name,
+            Campaign.description,
+            Campaign.goal,
+            Campaign.pledged,
+            Campaign.outcome,
+            Campaign.backers_count,
+            Campaign.country,
+            Campaign.currency,
+            Campaign.launched_date,
+            Campaign.end_date,
+            Campaign.staff_pick,
+            Campaign.spotlight,
+            Category.category,
+            Subcategory.subcategory
+        ).join(
+            Campaign, Contact.contact_id == Campaign.contact_id
+        ).join(
+            Category, Campaign.category_id == Category.category_id
+        ).join(
+            Subcategory, Campaign.subcategory_id == Subcategory.subcategory_id
+        ).all()
 
         # close session
         session.close()
@@ -56,16 +117,89 @@ class SQLHelper():
         data = analysis1_df.to_dict(orient="records")
         return (data)
 
-    def query_analysis1_sql(self):
-        # Find the most recent date in the data set.
-        query = """
-                SELECT 
-                FROM 
-                WHERE 
-                ORDER BY ;
-                """
+    # def reached_goal_sql(self):
+    #     # Find the most recent date in the data set.
+    #     query = """
+    #             SELECT
+    #                 cont.first_name,
+    #                 cont.last_name,
+    #                 cont.email,
+    #                 camp.company_name,
+    #                 camp.description,
+    #                 camp.goal,
+    #                 camp.pledged,
+    #                 camp.outcome,
+    #                 camp.backers_count,
+    #                 camp.country,
+    #                 camp.currency,
+    #                 camp.launched_date,
+    #                 camp.end_date,
+    #                 camp.staff_pick,
+    #                 camp.spotlight,
+    #                 cat.category,
+    #                 sub.subcategory
+    #             FROM
+    #                 public.contact cont
+    #             INNER JOIN public.campaign camp
+    #                 ON cont.contact_id = camp.contact_id
+    #             INNER JOIN public.category cat
+    #                 ON camp.category_id = cat.category_id
+    #             INNER JOIN public.subcategory sub
+    #                 ON camp.subcategory_id = sub.subcategory_id
+    #             WHERE
+    #                 (staff_pick = TRUE OR spotlight = TRUE)
+    #                 AND pledged >= goal
+    #             ;
+    #             """
 
-        # Save the query results as a Pandas DataFrame
-        analysis1_df = pd.read_sql(text(query), con=self.engine)
-        data = analysis1_df.to_dict(orient="records")
-        return (data)
+    #     # Save the query results as a Pandas DataFrame
+    #     reached_goal_sql_df = pd.read_sql(text(query), con=self.engine)
+    #     data = reached_goal_sql_df.to_dict(orient="records")
+    #     return (data)
+    
+    # def reached_goal_orm(self):
+    #     # Save reference to the table
+    #     reached_goal_orm = self.Base.classes.analysis1
+
+    #     # Create our session (link) from Python to the DB
+    #     session = Session(self.engine)
+
+
+
+    #     # close session
+    #     session.close()
+
+    #     # Convert the query results to a Dictionary
+    #     data = reached_goal_orm_df.to_dict(orient="records")
+    #     return (data)
+    
+    #     def query_analysis1_sql(self):
+    #     # Find the most recent date in the data set.
+    #     query = """
+    #             SELECT 
+    #             FROM 
+    #             WHERE 
+    #             ORDER BY ;
+    #             """
+
+    #     # Save the query results as a Pandas DataFrame
+    #     analysis1_df = pd.read_sql(text(query), con=self.engine)
+    #     data = analysis1_df.to_dict(orient="records")
+    #     return (data)
+    
+    # def query_analysis1_orm(self):
+    #     # Save reference to the table
+    #     analysis1 = self.Base.classes.analysis1
+
+    #     # Create our session (link) from Python to the DB
+    #     session = Session(self.engine)
+
+
+
+    #     # close session
+    #     session.close()
+
+    #     # Convert the query results to a Dictionary
+    #     data = analysis1_df.to_dict(orient="records")
+    #     return (data)
+

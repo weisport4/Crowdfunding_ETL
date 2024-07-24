991e9d6344072ef8cb1abc5de692b60bca85b9fe
Carlos Ruiz <Carlos.Ruiz@AIDecisions.com>
2024-07-24 11:34:29 -0400

    added indexes and QAed data upload into PostgreSQL

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,6 +1,6 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
+-- Modified by the team to include relationship integrity and indexes
 
 CREATE TABLE "category" (
     "category_id" VARCHAR(10)   NOT NULL,
@@ -62,3 +62,55 @@ REFERENCES "category" ("category_id");
 
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
+
+-- Indexes for Primary Keys
+CREATE UNIQUE INDEX idx_cat_id
+    ON public.category (category_id)
+    TABLESPACE pg_default;
+
+CREATE UNIQUE INDEX idx_subcat_id
+    ON public.subcategory (subcategory_id)
+    TABLESPACE pg_default;
+
+CREATE UNIQUE INDEX idx_cont_id
+    ON public.contact (contact_id)
+    TABLESPACE pg_default;
+
+CREATE UNIQUE INDEX idx_camp_id
+    ON public.campaign (funding_id)
+    TABLESPACE pg_default;
+
+-- Indexes for foreign keys and where clauses
+CREATE INDEX isx_camp_outcome_id
+    ON public.campaign (outcome ASC NULLS LAST)
+    WITH (deduplicate_items=True)
+    TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.campaign
+    CLUSTER ON isx_camp_outcome_id;
+
+CREATE INDEX isx_camp_country_id
+    ON public.campaign (country ASC NULLS LAST)
+    WITH (deduplicate_items=True)
+    TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.campaign
+    CLUSTER ON isx_camp_country_id;
+
+CREATE INDEX isx_camp_staff_id
+    ON public.campaign (staff_pick ASC NULLS LAST)
+    WITH (deduplicate_items=True)
+    TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.campaign
+    CLUSTER ON isx_camp_staff_id;
+
+CREATE INDEX isx_camp_spotlight_id
+    ON public.campaign (spotlight ASC NULLS LAST)
+    WITH (deduplicate_items=True)
+    TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.campaign
+    CLUSTER ON isx_camp_spotlight_id;
+
+-- Verification of the table creation, is on a separate file called "select_sql.sql"
